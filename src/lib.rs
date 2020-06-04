f49691174ae7199294f2e94a532830606f6ea4ff
mattsse <matthias.seitz@tum.de>
2020-06-04 22:45:34 +0200

    add hypertrie member

---
+++ src/lib.rs
@@ -0,0 +1,150 @@
+use async_std::fs;
+use async_std::prelude::*;
+use futures::FutureExt;
+use hypercore::{generate_keypair, Feed, FeedBuilder, PublicKey, Storage, Store};
+use rand::RngCore;
+use random_access_disk::RandomAccessDisk;
+use random_access_storage::RandomAccess;
+use std::fmt;
+use std::path::{Path, PathBuf};
+
+mod hypertrie_proto {
+    include!(concat!(env!("OUT_DIR"), "/hypertrie_pb.rs"));
+}
+mod storage;
+mod trie;
+
+const HYPERCORE: &'static [u8] = b"hypercore";
+
+const MASTER_KEY_FILENAME: &'static str = "master_key";
+
+/// Hyperdrive indexes filenames into a Hypercore. To avoid having to scan
+/// through this entire Hypercore when you want to find a specific file or
+/// folder, filenames are indexed using an append-only hash trie, which are
+/// called Hypertrie. The hash trie basically functions as a fast append-only
+/// key value store with listable folders.
+pub struct Hyperdrive<TConent, TMetadata = TConent>
+where
+    TConent: RandomAccess<Error = Box<dyn std::error::Error + Send + Sync>> + fmt::Debug + Send,
+    TMetadata: RandomAccess<Error = Box<dyn std::error::Error + Send + Sync>> + fmt::Debug + Send,
+{
+    dir: PathBuf,
+    content: Feed<TConent>,
+    metadata: Feed<TMetadata>,
+}
+
+// TODO metadata = `db` in js and has its own feed
+//
+
+impl<TContent, TMetadata> Hyperdrive<TContent, TMetadata>
+where
+    TContent: RandomAccess<Error = Box<dyn std::error::Error + Send + Sync>> + fmt::Debug + Send,
+    TMetadata: RandomAccess<Error = Box<dyn std::error::Error + Send + Sync>> + fmt::Debug + Send,
+{
+    /// If the hyperdrive has already been created, wait for the db (metadata
+    /// feed) to load. If the metadata feed is writable, we can immediately load
+    /// the content feed from its private key. (Otherwise, we need to read the
+    /// feed's metadata block first)
+    pub async fn restore(keypair: String) {}
+
+    pub async fn write_file(&self) {}
+
+    pub async fn read_file(&self) {}
+
+    async fn mkdir(&self) {}
+
+    pub fn is_writable(&self) -> bool {
+        unimplemented!()
+    }
+
+    pub async fn update(&self) {}
+
+    fn storage_root(&self) {}
+
+    async fn ready(&mut self) {}
+
+    // TODO API: https://awesome.datproject.org/hyperdrive#
+}
+
+impl Hyperdrive<RandomAccessDisk> {
+    /// Create a new instance that persists to disk at the location of `dir`.
+    pub async fn open(dir: impl AsRef<Path>) -> anyhow::Result<()> {
+        let dir = dir.as_ref();
+        if !dir.exists() {
+            fs::create_dir_all(dir).await?;
+        } else {
+            assert!(dir.is_dir());
+        }
+
+        // write master key: random 32 bytes
+        let mut master_key = [0u8; 32];
+        fill_random_bytes(&mut master_key);
+
+        let mut master_file = fs::File::create(dir.join(MASTER_KEY_FILENAME)).await?;
+        master_file.write_all(&master_key).await?;
+
+        // create metadata and content feed
+
+        Ok(())
+    }
+
+    async fn new_feed(dir: impl AsRef<Path>) -> anyhow::Result<Feed<RandomAccessDisk>> {
+        let dir = dir.as_ref();
+        let keypair = generate_keypair();
+
+        let storage = |storage: Store| {
+            let name = match storage {
+                Store::Tree => "tree",
+                Store::Data => "data",
+                Store::Bitfield => "bitfield",
+                Store::Signatures => "signatures",
+                Store::Keypair => "key",
+            };
+
+            let dkey = hex::encode(&discovery_key(&keypair.public));
+
+            // storage root is `<dir>/dkey[0..2]/dkey[2..4]/<dkey>`
+            let storage_root = Path::new(&dkey.as_str()[0..2])
+                .join(&dkey.as_str()[2..4])
+                .join(dkey);
+
+            let file = dir.join(storage_root).join(name);
+
+            RandomAccessDisk::open(file).boxed()
+        };
+
+        let mut storage = Storage::new(storage).await?;
+        storage.write_public_key(&keypair.public).await?;
+        storage.write_secret_key(&keypair.secret).await?;
+
+        Ok(FeedBuilder::new(keypair.public, storage)
+            .secret_key(keypair.secret)
+            .build()?)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct HyperdriveBuilder {
+    key: String,
+    sparse: bool,
+    namespace: Option<String>,
+    key_pair: Option<()>,
+}
+
+// TODO impl AsyncRead AsyncWrite
+
+struct Feed2 {}
+
+fn fill_random_bytes(dest: &mut [u8]) {
+    use rand::rngs::{OsRng, StdRng};
+    use rand::SeedableRng;
+    let mut rng = StdRng::from_rng(OsRng::default()).unwrap();
+    rng.fill_bytes(dest)
+}
+
+/// Calculate a hypercore's discovery key (32 bytes) using the BLAKE2b hashing
+/// function, keyed with the public key (as 32 bytes, not 64 hexadecimal
+/// characters), to hash the word “hypercore”
+fn discovery_key(publickey: &PublicKey) -> blake2_rfc::blake2b::Blake2bResult {
+    blake2_rfc::blake2b::blake2b(32, publickey.as_ref(), HYPERCORE)
+}
---
+++ src/trie/mod.rs
@@ -0,0 +1,130 @@
+//! Distributed single writer key/value store
+//!Uses a rolling hash array mapped trie to index key/value data on top of a hypercore.
+use crate::discovery_key;
+use crate::hypertrie_proto as proto;
+use anyhow::anyhow;
+use hypercore::{Feed, PublicKey, SecretKey};
+use prost::Message as ProtoMessage;
+use random_access_storage::RandomAccess;
+use std::fmt;
+use std::fmt::Debug;
+
+pub mod batch;
+pub mod delete;
+pub mod diff;
+pub mod get;
+pub mod history;
+pub mod put;
+
+struct MountableHyperTrie<T>
+where
+    T: RandomAccess<Error = Box<dyn std::error::Error + Send + Sync>> + fmt::Debug,
+{
+    feed: Feed<T>,
+}
+
+impl<T> MountableHyperTrie<T>
+where
+    T: RandomAccess<Error = Box<dyn std::error::Error + Send + Sync>> + fmt::Debug,
+{
+    pub fn get_feed(&self) -> &Feed<T> {
+        &self.feed
+    }
+
+    pub fn get_feed_mut(&mut self) -> &mut Feed<T> {
+        &mut self.feed
+    }
+
+    pub async fn get(&self) {}
+
+    async fn mount(&self) {}
+
+    pub async fn put(&mut self) {}
+}
+
+struct HyperTrie<T>
+where
+    T: RandomAccess<Error = Box<dyn std::error::Error + Send + Sync>> + fmt::Debug,
+{
+    /// these are all from the metadata feed
+    feed: Feed<T>,
+    discovery_key: [u8; 32],
+    version: usize,
+    /// This is public key of the content feed
+    metadata: Option<PublicKey>,
+}
+
+impl<T> HyperTrie<T>
+where
+    T: RandomAccess<Error = Box<dyn std::error::Error + Send + Sync>> + Debug + Send,
+{
+    /// Returns the db public key. You need to pass this to other instances you want to replicate with.
+    pub fn key(&self) -> &PublicKey {
+        self.feed.public_key()
+    }
+
+    /// Returns the db discovery key. Can be used to find other db peers.
+    pub fn discovery_key(&self) -> blake2_rfc::blake2b::Blake2bResult {
+        discovery_key(self.key())
+    }
+
+    /// Returns a new db instance checked out at the version specified.
+    async fn checkout(self, version: String) {}
+
+    /// Returns a hypercore replication stream for the db. Pipe this together with another hypertrie instance.
+    async fn replicate(self) {}
+
+    /// Same as checkout but just returns the latest version as a checkout.
+    async fn snapshot(self) {
+        // self.checkout(self.version)
+    }
+
+    async fn head(self) {}
+
+    async fn diff(self) {}
+    /// Lookup a key. Returns a result node if found or `None` otherwise.
+    async fn get(&self) {}
+
+    /// Insert a value.
+    async fn put(&mut self) {}
+
+    /// Delete a key from the database.
+    async fn delete(&mut self) {}
+
+    async fn ready(&mut self) -> anyhow::Result<()> {
+        if self.feed.is_empty() {
+            let mut header = proto::Header::default();
+            header.r#type = "hypertrie".to_string();
+            header.metadata = self.metadata.map(|pk| pk.as_bytes().to_vec());
+        }
+
+        unimplemented!()
+    }
+
+    pub async fn get_metadata(&mut self) -> anyhow::Result<Option<PublicKey>> {
+        let data = self
+            .feed
+            .get(0)
+            .await?
+            .ok_or_else(|| anyhow!("No hypertrie header present."))?;
+        if let Some(meta) = proto::Header::decode(&*data)?.metadata {
+            Ok(Some(PublicKey::from_bytes(&meta)?))
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+// TODO impl watcher: channels or futures::stream::Stream?
+
+#[derive(Debug, Clone)]
+pub enum ValueEncoding {
+    Json,
+    Binary,
+}
+
+impl Default for ValueEncoding {
+    fn default() -> Self {
+        ValueEncoding::Binary
+    }
+}
