5619331b5f850edd4f2860974647ee96231ea9b7
mattsse <matthias.seitz@tum.de>
2020-06-13 01:27:10 +0200

    chore: extension draft

--- src/trie/extension.rs
+++ src/trie/extension.rs
@@ -6,6 +6,7 @@ pub const FLUSH_BATCH: u64 = 128;
 pub const MAX_PASSIVE_BATCH: u64 = 2048;
 pub const MAX_ACTIVE_BATCH: u64 = MAX_PASSIVE_BATCH + FLUSH_BATCH;
 
+#[derive(Debug, Clone, PartialEq)]
 pub struct HypertrieExtension {
     extension: proto::Extension,
 }
