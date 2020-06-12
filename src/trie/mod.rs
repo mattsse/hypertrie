e55fd81b3fe69d785ffaf55a953b80313ed2fcb0
mattsse <matthias.seitz@tum.de>
2020-06-13 01:24:48 +0200

    test: multiple put/get test

--- src/trie/mod.rs
+++ src/trie/mod.rs
@@ -446,7 +446,6 @@ mod tests {
     #[async_std::test]
     async fn get_on_empty() -> Result<(), Box<dyn std::error::Error>> {
         let mut trie = HyperTrieBuilder::default().ram().await?;
-
         let get = trie.get("hello").await?;
         assert!(get.is_none());
 
@@ -458,7 +457,6 @@ mod tests {
         let mut trie = HyperTrieBuilder::default().ram().await?;
 
         let put = trie.put("/hello", b"world").await?;
-
         let get = trie.get("/hello").await?.unwrap();
         assert_eq!(put, get);
 
@@ -476,29 +474,26 @@ mod tests {
         let world_put = trie.put("world", b"hello").await?;
 
         let hello_get = trie.get("hello").await?.unwrap();
-        // let world_get = trie.get("world").await?.unwrap();
+        let world_get = trie.get("world").await?.unwrap();
 
-        // assert_eq!(hello_put, hello_get);
-        // assert_eq!(world_put, world_get);
+        assert_eq!(hello_put, hello_get);
+        assert_eq!(world_put, world_get);
 
         Ok(())
     }
 
     #[test]
     fn encode_decode_trie() {
-
         let trie = Trie(vec![]);
         let buf = trie.encode();
         assert_eq!(buf, vec![0]);
         let decoded = Trie::decode(&*buf);
         assert_eq!(trie, decoded);
 
-        let trie = Trie(
-            vec![None, Some(vec![None, Some(1)])]
-        );
+        let trie = Trie(vec![None, Some(vec![None, Some(1)])]);
         let buf = trie.encode();
         assert_eq!(buf.len(), 4);
-        assert_eq!(buf, vec![2,1,2,1]);
+        assert_eq!(buf, vec![2, 1, 2, 1]);
 
         let decoded = Trie::decode(&*buf);
         assert_eq!(trie, decoded);
