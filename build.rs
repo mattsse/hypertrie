5ee2309f70dddfd4689fbc493c99f283acfbe04e
mattsse <matthias.seitz@tum.de>
2020-06-18 21:17:59 +0200

    refactor: merge compile_protos calls

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
 fn main() {
-    prost_build::compile_protos(&["src/hypertrie_pb.proto"], &["src"]).unwrap();
-    prost_build::compile_protos(&["src/mount_pb.proto"], &["src"]).unwrap();
+    prost_build::compile_protos(&["src/hypertrie_pb.proto", "src/mount_pb.proto"], &["src"])
+        .unwrap();
 }
